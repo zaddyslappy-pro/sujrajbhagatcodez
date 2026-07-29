5525cd42cc7dc255f8626d6f7ca83ce8f20be2ed
zaddyslappy-pro <zaddyslappy@gmail.com>
2026-07-29 23:45:56 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (15.68%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int tribonacci(int n) {
+        int[] dp = new int[n + 1];
+
+        Arrays.fill(dp, -1);
+
+        return helper(n, dp);
+    }
+
+    private int helper(int n, int[] dp) {
+
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+
+        dp[n] = helper(n - 1, dp)
+              + helper(n - 2, dp)
+              + helper(n - 3, dp);
+
+        return dp[n];
+    }
+}
